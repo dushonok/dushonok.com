24b1a3eef6e7a4fafda23627e1ea05d109f3a7eb
dushonok <nadezhda18@gmail.com>
2015-03-22 16:10:46 -0400

    enqueue the parent and child theme stylesheets

--- EnfoldChildTheme/functions.php
+++ EnfoldChildTheme/functions.php
@@ -5,3 +5,12 @@ Enfold child Theme functions
 
 these funcs will replace the parent theme funcs with the same name
 */
+
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    wp_enqueue_style( 'child-style',
+        get_stylesheet_directory_uri() . '/style.css',
+        array('parent-style')
+    );
+}
